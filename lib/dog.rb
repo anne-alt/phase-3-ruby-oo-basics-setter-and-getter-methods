a4dbb606225297c06aa9014806d68013dbd7d67c
Anne Ogongo <anneogongo98@gmail.com>
2022-10-27 20:43:50 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
+    # attr_reader :name
+    # attr_accessor :breed
+
+    # def initialize(name, breed)
+    #     @name = name
+    #     @breed = breed
+    # end
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
 
 end
